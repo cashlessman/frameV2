bdf83199d3a38d017b161559996e1a9c0320dc07
cashlessman <cashlessman@proton.me>
2024-12-17 21:06:02 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import App from "./app";
 
-const appUrl = process.env.NEXT_PUBLIC_URL;
+const appUrl = "https://around-v2.vercel.app/";
 
 const frame = {
   version: "next",
